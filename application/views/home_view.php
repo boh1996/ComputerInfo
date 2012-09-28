83ed0451eb6974dad2397c14ee246e4a22574a4e
fredefl <frederiklassen@gmail.com>
2012-09-28 22:38:39 +0200

    This is just the start of da shit.

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -1,19 +1,92 @@
 <html>
 	<head>
 		<title>ComputerInfo - Home</title>
+		<!--
 		<link rel="stylesheet" type="text/css" href="<?php echo $asset_url; ?>bootstrap/css/bootstrap.min.css">
 		<link rel="stylesheet" type="text/css" href="<?php echo $asset_url; ?>bootstrap/css/bootstrap-responsive.min.css">
+		-->
+		<link rel="stylesheet" type="text/css" href="http://127.0.0.1/ci/assets/bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="http://127.0.0.1/ci/assets/bootstrap/css/bootstrap-responsive.min.css">
 	</head>
 
 	<body>
+		<!--
 		<?php 
 			if ($dev_mode) {
 				echo '<script type="text/javascript" src="'.$asset_url.'js/jquery.min.js"></script>';
 			} else {
 				echo '<script type="text/javascript" src="'.$jquery_url.'"></script>';
 			}
-		?>
+		?>-->
+
+		<style type="text/css">
+			body {
+				padding-top: 60px;
+				padding-bottom: 40px;
+			}
+			.clearfix:after {
+			     visibility: hidden;
+			     display: block;
+			     font-size: 0;
+			     line-height: 0;
+			     content: " ";
+			     clear: both;
+			     height: 0;
+			     width: 0;
+			     }
+			.clearfix { display: inline-block; }
+			/* start commented backslash hack \*/
+			* html .clearfix { height: 1%; }
+			.clearfix { display: block; }
+			/* close commented backslash hack */
+		</style>
+
+		<div class="navbar navbar-fixed-top">
+			<div class="navbar-inner">
+				<div class="container">
+					<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</a>
+					<a class="brand" href="#">ComputerInfo</a>
+					<div class="nav-collapse collapse">
+						<ul class="nav">
+							<li class="active"><a href="#">Home</a></li>
+							<li><a href="#about">About</a></li>
+							<li class="dropdown">
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown">Login <b class="caret"></b></a>
+								<ul class="dropdown-menu">
+									<li><a href="#">Action</a></li>
+									<li><a href="#">Another action</a></li>
+								</ul>
+							</li>
+						</ul>
+					</div><!--/.nav-collapse -->
+				</div>
+			</div>
+		</div>
+
+		<div class="container">
+			<div class="row">
+				<div class="span12"><h1>ComputerInfo - The source of information </h1></div>
+				<div class="span12">
+					<h4>With CoumputerInfo you've got all the information you need at your fingertips, right when you need them most.</h4>
+				</div>
+				<div class="span5">
+					Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,
+				</div>
+				<div class="span7" style="background: #AAAAAA; height: inherit;">
+					
+				</div>
+
+		
+		<!--
 		<script type="text/javascript" src="<?php echo $asset_url; ?>bootstrap/js/bootstrap.js"></script>
 		<script type="text/javascript" src="<?php echo $asset_url; ?>js/jquery.history.js"></script>
+		-->
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+		<script type="text/javascript" src="http://127.0.0.1/ci/assets/bootstrap/js/bootstrap.js"></script>
+		<script type="text/javascript" src="http://127.0.0.1/ci/assets/js/jquery.history.js"></script>
 	</body>
 </html>
