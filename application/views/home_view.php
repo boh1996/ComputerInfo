1a2246fea59e9d832cdde1003fb98da7cdcbf51e
fredefl <frederiklassen@gmail.com>
2012-09-29 14:55:27 +0200

    Added product showcase/carousel.

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -21,19 +21,19 @@
 
 		<style type="text/css">
 			body {
-				padding-top: 60px;
+				padding-top: 45px;
 				padding-bottom: 40px;
 			}
 			.clearfix:after {
-			     visibility: hidden;
-			     display: block;
-			     font-size: 0;
-			     line-height: 0;
-			     content: " ";
-			     clear: both;
-			     height: 0;
-			     width: 0;
-			     }
+				 visibility: hidden;
+				 display: block;
+				 font-size: 0;
+				 line-height: 0;
+				 content: " ";
+				 clear: both;
+				 height: 0;
+				 width: 0;
+				 }
 			.clearfix { display: inline-block; }
 			/* start commented backslash hack \*/
 			* html .clearfix { height: 1%; }
@@ -73,11 +73,34 @@
 				<div class="span12">
 					<h4>With CoumputerInfo you've got all the information you need at your fingertips, right when you need them most.</h4>
 				</div>
+				<div class="span12">
+
+				</div>
 				<div class="span5">
 					Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,
 				</div>
-				<div class="span7" style="background: #AAAAAA; height: inherit;">
-					
+				<div class="span7">
+					<div id="myCarousel" class="carousel slide">
+						<div class="carousel-inner">
+							<div class="active item">
+								<img src="http://127.0.0.1/ci/assets/images/Home.png"/>
+								<div class="carousel-caption">
+									<h4>Modern easy to use control panel</h4>
+									<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+								</div>
+							</div>
+							<div class="item">
+								<img src="http://127.0.0.1/ci/assets/images/Android.png"/>
+								<div class="carousel-caption">
+									<h4>All new fresh Android app</h4>
+									<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+								</div>
+							</div>
+						</div>
+						<!-- Carousel nav -->
+						<a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+						<a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+					</div>
 				</div>
 
 		
