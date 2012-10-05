5feea7705b57a0597c1001009505f055bc441277
Bo Thomsen <boh1996@gmail.com>
2012-10-05 17:51:08 +0200

    Placeholder

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -47,7 +47,7 @@ if(!isset($jquery_url))
 								<a class="dropdown-toggle" href="#" data-toggle="dropdown">Sign In <strong class="caret"></strong></a>
 								<div class="dropdown-menu" style="padding: 15px; padding-bottom: 0px;">
 									<form method="post" action="<?php echo $base_url; ?>login/check" method="post" accept-charset="UTF-8">
-										<input style="margin-bottom: 15px;" type="text" placeholder="Username" id="username" name="username"/>
+										<input style="margin-bottom: 15px;" type="text" placeholder="Username or Email" id="username" name="username"/>
 										<input style="margin-bottom: 15px;" type="password" placeholder="Password" id="password" name="password" />
 										<input style="float: left; margin-right: 10px;" type="checkbox" name="remember-me" id="remember-me" value="1" />
 										<label class="string optional" for="user_remember_me"> Remember me</label>
