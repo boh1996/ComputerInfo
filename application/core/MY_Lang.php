4eb5f4a269aedf70586fd9c104cda26716ed41a1
Bo Thomsen <boh1996@gmail.com>
2013-01-27 23:43:46 +0100

    Mylang fixes
    Signed-off-by: Bo Thomsen <boh1996@gmail.com>

--- application/core/MY_Lang.php
+++ application/core/MY_Lang.php
@@ -1,6 +1,14 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 class MY_Lang extends CI_Lang{
 
+    /**
+     * This function fetches a line from the current selected langauge
+     * @since 1.0
+     * @access public
+     * @param  string $line   The language key to fetch0
+     * @param  array $params Optional "tokens/variables" to replace in the line
+     * @return string
+     */
     public function line ($line, $params = null) {
 
         $return = parent::line($line);
@@ -16,6 +24,14 @@ class MY_Lang extends CI_Lang{
 
     }
     
+    /**
+     * This function replaces the specified tokens in the language line
+     * @since 1.0
+     * @access public
+     * @param  stirng $str    The string to replace the tokesn in
+     * @param  array $params The tokens to replace and their replacements
+     * @return string
+     */
     private function _ni_line($str, $params){
         $return = $str;
         
