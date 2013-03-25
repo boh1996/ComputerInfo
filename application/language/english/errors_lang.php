858a6104e4e6e9c09e77039d830477f80c49bdf6
Bo Thomsen <boh1996@gmail.com>
2013-03-25 01:11:00 +0100

    Line spacing

--- application/language/english/errors_lang.php
+++ application/language/english/errors_lang.php
@@ -1,8 +1,6 @@
 <?php
 $lang["error_encountered"] = "Sorry an error encountered!";
-
 $lang["error_sorry_no_object_found"] = "Sorry no object found at this URL!";
-
 $lang["error_page_not_found"] = "Page not found!";
 $lang["error_an_error_occured"] = "An error occured!";
 ?>
